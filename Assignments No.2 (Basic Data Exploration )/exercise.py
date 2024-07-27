a3bb8f23b953c8da73006d53ff69c094cbad059f
MJParviz <mj.parviz2005@gmail.com>
2024-07-27 19:35:34 +0330

    Update exercise.py

--- Assignments No.2 (Basic Data Exploration )/exercise.py
+++ Assignments No.2 (Basic Data Exploration )/exercise.py
@@ -1,11 +1,12 @@
-#exercise 1
+#----------exersice 1 ----------
 import pandas as pd
 
 #load and read our dataset
 iowa_file_path = '../input/home-data-for-ml-course/train.csv'
 home_data = pd.read_csv(iowa_file_path)
 
-#exersice 2
+
+#----------exersice 2 ----------
 #describe our dataset with Pandas DataFrame feature
 kaka = pd.DataFrame.describe(home_data)
 print(kaka)
