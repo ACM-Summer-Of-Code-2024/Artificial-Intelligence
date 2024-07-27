4bf5d37e2a4aeeaf772cc480a524ab1aece5d729
MJParviz <mj.parviz2005@gmail.com>
2024-07-27 19:35:07 +0330

    Update exersice.py

--- Assignments No.3 (Your First Machine Learning Model)/exersice.py
+++ Assignments No.3 (Your First Machine Learning Model)/exersice.py
@@ -5,12 +5,16 @@ import pandas as pd
 iowa_file_path = '../input/home-data-for-ml-course/train.csv'
 home_data = pd.read_csv(iowa_file_path)
 
+
+#----------exersice 1 ----------
 # print the list of columns in the dataset to find the name of the prediction target
 print(home_data.columns)
 
 #select and save desired column
 y = home_data.SalePrice
 
+
+#----------exersice 2 ----------
 # Create the list of features we want
 feature_names = ['LotArea','YearBuilt','1stFlrSF','2ndFlrSF','FullBath','BedroomAbvGr','TotRmsAbvGrd']
 
@@ -24,6 +28,8 @@ print(X.describe())
 # print the top few lines
 print(X.head())
 
+
+#----------exersice 3 ----------
 from sklearn.tree import DecisionTreeRegressor
 #specify the model. 
 #For model reproducibility, set a numeric value for random_state when specifying the model
@@ -32,10 +38,11 @@ iowa_model = DecisionTreeRegressor(random_state=1)
 # Fit the model
 iowa_model.fit(X,y)
 
+
+#----------exersice 4 ----------
 #predict sales price of home using predict()
 predictions = iowa_model.predict(X)
 print(predictions)
-
 
 #review resault 
 print(home_data.SalePrice)
